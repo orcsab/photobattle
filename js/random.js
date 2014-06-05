e504589e03e02d5f4afcd694eff2c690d695c23d
orcsab <orcsab@gmail.com>
2014-06-05 19:44:03 +0800

    random page is now dymanic, although broken

---
+++ js/random.js
@@ -0,0 +1,22 @@
+$(function () {
+  var keys = [];
+  for (var k in images)
+    keys.push(k);
+  console.log('keys = ' + keys);
+  console.log('keys length = ' + keys.length);
+  var i = Math.floor ((Math.random * keys.length) + 1);
+  console.log('selecting key ' + i);
+  var key = keys[i];
+
+  console.log('selected key = ' + key);
+  $('#img-src').attr('src', key);
+  $('#img-src').attr('alt', images[key].description);
+  $('#rtg-funny').text(images[key].rating.funny);
+  $('#rtg-creative').text(images[key].rating.creative);
+  $('#rtg-amazing').text(images[key].rating.amazing);
+  $('#rtg-overall').text(images[key].rating.overall);
+  $('#img-owner').text(images[key].owner);
+  $('#img-date').text(images[key].date);
+  $('#img-location').text('undefined');
+  $('#img-description').text(images[key].description);
+});
